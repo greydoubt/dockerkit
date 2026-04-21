b2259742047c4bc1ec57805273364fe9fafbeb2b
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-21 01:08:03 -0700

    Create rifter.sh

---
+++ rifter.sh
@@ -0,0 +1,24 @@
+fosslinux@tuts:~$ sudo apt install spectrwm -y
+fosslinux@tuts:~$ spectrwm -v#spectrwm 3.5.1
+fosslinux@tuts:~$ sudo apt install herbstluftwm -y
+fosslinux@tuts:~$ herbstluftwm --version#herbstluftwm 0.9.5
+fosslinux@tuts:~$ sudo apt install dwm -y
+fosslinux@tuts:~$ dwm -v#dwm-6.4
+fosslinux@tuts:~$ sudo apt install xmonad -y
+fosslinux@tuts:~$ xmonad --version#xmonad 0.17.0
+fosslinux@tuts:~$ sudo apt install bspwm sxhkd -y
+fosslinux@tuts:~$ bspwm -v#0.9.10
+fosslinux@tuts:~$ sudo apt install qtile -y
+fosslinux@tuts:~$ qtile --version#0.24.0
+fosslinux@tuts:~$ sudo apt install awesome -y
+fosslinux@tuts:~$ awesome --version#awesome v4.3 (Too Hot)
+fosslinux@tuts:~$ sudo apt install i3 -y#add tiling layer
+fosslinux@tuts:~$ i3 --version#i3 version 4.23 (2023-10-29)
+fosslinux@tuts:~$ sudo apt install niri -y
+fosslinux@tuts:~$ niri --version#niri 0.1.7
+fosslinux@tuts:~$ sudo apt install river -y
+fosslinux@tuts:~$ river --version#river version 0.2.4
+fosslinux@tuts:~$ sudo apt install sway -y
+fosslinux@tuts:~$ sway --version#sway version 1.9 (Mar 12 2024, branch 'master')
+fosslinux@tuts:~$ sudo apt update && sudo apt install hyprland -y
+fosslinux@tuts:~$ Hyprland --version#Hyprland, built from branch at commit (v0.35.0-60-ga0b6a7a0).
