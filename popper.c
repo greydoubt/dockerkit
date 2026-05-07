54e476afbc0a68465b1ff5e2639544833022fcec
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-05-06 22:21:51 -0700

    Update popper.c

--- popper.c
+++ popper.c
@@ -1,5 +1,4 @@
 #ifndef lint
-static char copyright[] = "Copyright (c) 1990 Regents of the University of California.\nAll rights reserved.\n";
 static char SccsId[] = "@(#)@(#)popper.c	2.1  2.1 3/18/91";
 #endif not lint
 
